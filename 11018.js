6f395aca6791287f03fe418d0a64d4a0ba73a279
Rokee <Lovokey@163.com>
2018-10-25 17:05:44 +0800

    Create 11018.js

---
+++ 11018.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "湖北工业大学",
+    id: "11018",
+    link: [ {
+        name: "湖工主页",
+        url: "https://www.tsinghua.edu.cn/"
+    }, {
+        name: "个人门户",
+        url: "https://sso.hbut.edu.cn:7002/cas/login?null"
+    }, {
+        name: "图书馆",
+        url: "http://lib.hbut.edu.cn/index.html"
+    }, {
+        name: "湖工邮箱",
+        url: "http://xmail.hbut.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://run.hbut.edu.cn/"
+    }]
+};
